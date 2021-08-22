d70386bce60120b6063855ff6d7db1f3f4d8ea43
David Alexandru Ilie <david@davidilie.com>
2021-08-22 19:45:11 +0200

    remove unused env stuff

--- server/src/types/env.d.ts
+++ server/src/types/env.d.ts
@@ -1,9 +1,5 @@
 declare namespace NodeJS {
     interface ProcessEnv {
         PORT: string;
-        MINIO_ENDPOINT: string;
-        MINIO_ACCESS_KEY: string;
-        MINIO_SECRET_KEY: string;
-        MINIO_BUCKET_NAME: string;
     }
 }
