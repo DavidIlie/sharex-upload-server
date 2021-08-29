5fa9332454337669d57169e23398d58810973824
David Alexandru Ilie <david@davidilie.com>
2021-08-29 19:01:03 +0200

    fixes

--- web/src/ui/form/Select.tsx
+++ web/src/ui/form/Select.tsx
@@ -8,7 +8,7 @@ const Select = ({ required, children, ...rest }: InputProps): JSX.Element => {
         <div className="relative inline-block w-full">
             <select
                 {...rest}
-                className="w-full h-10 pl-3 pr-6 text-base bg-gray-800 border-dark-gray-800 text-gray-200 focus:outline-none focus:border-dark-gray-700 focus:ring-opacity-50 border rounded-lg appearance-none"
+                className="w-full h-10 pl-3 pr-6 text-base bg-gray-800 border-gray-900 border-2 text-gray-200 focus:outline-none focus:border-dark-gray-700 focus:ring-opacity-50 rounded-lg appearance-none"
             >
                 {children}
             </select>
