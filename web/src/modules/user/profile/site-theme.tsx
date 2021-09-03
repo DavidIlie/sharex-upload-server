9c359027d5b0a869c330f9312f63bd6a91de9c44
David Ilie <david@davidilie.com>
2021-09-03 18:55:51 +0200

    removed submit type in change theme button

--- web/src/modules/user/profile/site-theme.tsx
+++ web/src/modules/user/profile/site-theme.tsx
@@ -23,7 +23,6 @@ const SiteThemeModule = (): JSX.Element => {
                     </p>
 
                     <button
-                        type="submit"
                         className="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-dark-gray-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150 mt-4"
                         onClick={() => updateTheme()}
                     >
