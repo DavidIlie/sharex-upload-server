c37f963be2fb06e7e7df98c106c99e7c48da42e4
David Ilie <david@davidilie.com>
2021-09-07 13:21:05 +0200

    added way to automatically create folder if it doesn't exist, and also delete the upload table if the folder did not exist

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,10 @@ import morgan from "morgan";
 import { createConnection } from "typeorm";
 import chalk from "chalk";
 import cookieParser from "cookie-parser";
+import fs from "fs";
+
+import { uploadDir } from "./lib/filesystem";
+import { Uploads } from "./entities/Uploads";
 
 import * as middlewares from "./middleware";
 import api from "./routes";
@@ -14,6 +18,17 @@ const main = async () => {
     console.log(chalk`{bold.yellow running migrations}`);
     await conn.runMigrations();
     console.log(chalk`{bold.green migrations finished!}`);
+
+    if (!fs.existsSync(uploadDir)) {
+        console.log(chalk`{bold.cyan created uploads folder}`);
+        fs.mkdirSync(uploadDir);
+
+        const uploads = await Uploads.find();
+        if (uploads) {
+            await Uploads.remove(uploads);
+            console.log(chalk`{bold.blue removed all uploads in DB}`);
+        }
+    }
 
     const app = express();
 
