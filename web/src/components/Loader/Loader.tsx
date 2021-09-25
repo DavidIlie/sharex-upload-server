29030bbf43d9d1277083ca20f7c979c9d0f37810
David Alexandru Ilie <david@davidilie.com>
2021-09-25 10:12:58 +0200

    (chore): removed loading meta and other meta from showing on file so that maybe discord can see the file meta

--- web/src/components/Loader/Loader.tsx
+++ web/src/components/Loader/Loader.tsx
@@ -1,10 +1,14 @@
 import { NextSeo } from "next-seo";
+import { useRouter } from "next/router";
 import { Fade } from "react-awesome-reveal";
 
 const Loader = (): JSX.Element => {
+    const router = useRouter();
+    const path = router.pathname;
+
     return (
         <>
-            <NextSeo title="Loading" />
+            {!path.startsWith("/f") && <NextSeo title="Loading" />}
             <section className="h-screen flex justify-center items-center">
                 <Fade>
                     <div className="loading-loader">
