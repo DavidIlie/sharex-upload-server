8edf72ae1536a4236b69c832923ca24ccf3dcd40
David Ilie <david@davidilie.com>
2021-09-28 17:25:29 +0200

    (chore): change icons

--- web/src/components/DashboardStatCard/Icon.tsx
+++ web/src/components/DashboardStatCard/Icon.tsx
@@ -1,66 +1,42 @@
 interface IconTypes {
-    type: "total" | "file" | "image" | "text" | "metric";
+    type: "file" | "image" | "text" | "metric";
 }
 
 const Icon = ({ type }: IconTypes): JSX.Element => {
-    return type === "total" ? (
+    return type === "file" ? (
         <svg
-            className="sm:h-16 sm:w-16 h-12 w-12 text-gray-700 dark:text-dark-gray-400"
             xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-        >
-            <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"
-            />
-        </svg>
-    ) : type === "file" ? (
-        <svg
             className="sm:h-16 sm:w-16 h-12 w-12 text-gray-700 dark:text-dark-gray-400"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
+            viewBox="0 0 20 20"
+            fill="currentColor"
         >
-            <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2"
-            />
+            <path d="M9 2a2 2 0 00-2 2v8a2 2 0 002 2h6a2 2 0 002-2V6.414A2 2 0 0016.414 5L14 2.586A2 2 0 0012.586 2H9z" />
+            <path d="M3 8a2 2 0 012-2v10h8a2 2 0 01-2 2H5a2 2 0 01-2-2V8z" />
         </svg>
     ) : type === "image" ? (
         <svg
-            className="sm:h-16 sm:w-16 h-12 w-12 text-gray-700 dark:text-dark-gray-400"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
+            className="sm:h-16 sm:w-16 h-12 w-12 text-gray-700 dark:text-dark-gray-400"
+            viewBox="0 0 20 20"
+            fill="currentColor"
         >
             <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
-            ></path>
+                fillRule="evenodd"
+                d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z"
+                clipRule="evenodd"
+            />
         </svg>
     ) : type === "text" ? (
         <svg
-            className="sm:h-16 sm:w-16 h-12 w-12 text-gray-700 dark:text-dark-gray-400"
             xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
+            className="sm:h-16 sm:w-16 h-12 w-12 text-gray-700 dark:text-dark-gray-400"
+            viewBox="0 0 20 20"
+            fill="currentColor"
         >
             <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+                fillRule="evenodd"
+                d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z"
+                clipRule="evenodd"
             />
         </svg>
     ) : (
