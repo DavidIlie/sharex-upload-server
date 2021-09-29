f082736cd270dbf2840372f57a6605b0cbdceaac
David Ilie <david@davidilie.com>
2021-09-29 13:02:44 +0200

    (chores): #8

--- web/src/pages/t/[slug]/index.tsx
+++ web/src/pages/t/[slug]/index.tsx
@@ -1,4 +1,5 @@
 import { GetServerSideProps } from "next";
+import { useState } from "react";
 import { NextSeo } from "next-seo";
 import { useRouter } from "next/router";
 import { useTheme } from "next-themes";
@@ -27,6 +28,11 @@ const ViewFile = ({ message, file, text }: Props): JSX.Element => {
 
     const { theme } = useTheme();
 
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+
+    const words = text.split(/\s+/gu).length;
+    const lines = text.split(/\r\n|\r|\n/).length;
+
     return (
         <>
             <NextSeo
@@ -41,27 +47,77 @@ const ViewFile = ({ message, file, text }: Props): JSX.Element => {
                     type: "website",
                 }}
             />
-            <svg
-                className="w-8 h-8 text-blue-400 cursor-pointer"
-                //TODO: MAKE THIS FOLLOW YOU
-                style={{
-                    position: "fixed",
-                    bottom: "2rem",
-                    right: "4rem",
-                    zIndex: 500,
-                }}
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-            >
-                <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                ></path>
-            </svg>
+            <div className="absolute bottom-5 right-12" style={{ zIndex: 500 }}>
+                {isOpen && (
+                    <div className="absolute bottom-14 -right-2 p-4 w-64 bg-gray-900 text-white text-sm rounded shadow-md">
+                        <p className="pb-2 text-base font-bold text-center">
+                            File Information
+                        </p>
+                        <div className="divide-y divide-gray-800">
+                            <div className="flex justify-between">
+                                <span className="text-blue-400 font-semibold pr-2">
+                                    ID
+                                </span>
+                                <span className="overflow-hidden overflow-ellipsis">
+                                    {file.slug}
+                                </span>
+                            </div>
+
+                            <div className="flex justify-between">
+                                <span className="text-blue-400 font-semibold pr-2">
+                                    Name
+                                </span>
+                                <span className="overflow-hidden overflow-ellipsis">
+                                    {file.name}
+                                </span>
+                            </div>
+
+                            <div className="flex justify-between">
+                                <span className="text-blue-400 font-semibold pr-2">
+                                    Extension
+                                </span>
+                                <span className="overflow-hidden overflow-ellipsis">
+                                    {file.stats.extension}
+                                </span>
+                            </div>
+
+                            <div className="flex justify-between">
+                                <span className="text-blue-400 font-semibold pr-2">
+                                    Lines
+                                </span>
+                                <span className="overflow-hidden overflow-ellipsis">
+                                    {lines}
+                                </span>
+                            </div>
+
+                            <div className="flex justify-between">
+                                <span className="text-blue-400 font-semibold pr-2">
+                                    Words
+                                </span>
+                                <span className="overflow-hidden overflow-ellipsis">
+                                    {words}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                )}
+
+                <svg
+                    onClick={() => setIsOpen(!isOpen)}
+                    className="absolute bottom-3 right-3 w-8 h-8 text-blue-400 cursor-pointer"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                    ></path>
+                </svg>
+            </div>
             <div className="font-sans antialiased -my-2">
                 <SyntaxHighlighter
                     style={theme === "light" ? light : dark}
