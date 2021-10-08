ca4d0d45bd51abb579bb3a1b162cf54406a58c16
David Ilie <david@davidilie.com>
2021-10-08 16:43:55 +0200

    fix background changing colors on rerender

--- web/src/components/AppLayout/AppLayout.tsx
+++ web/src/components/AppLayout/AppLayout.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useShortcut } from "litkey";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
@@ -32,14 +33,32 @@ const AppLayout = ({ children }: LayoutProps): JSX.Element => {
     return (
         <>
             {dissmissDevAlert !== "true" && <DevelopmentModeAlertModule />}
+            <Background />
+            <div className="flex flex-col justify-between min-h-screen text-black dark:text-white pageBackground backdrop-filter backdrop-blur-sm dark:backdrop-blur-sm">
+                <ThemeSwitcher />
+                {children}
+                {!router.asPath.includes("/t/") && <Footer />}
+            </div>
+        </>
+    );
+};
+
+export default AppLayout;
+
+const Background = React.memo(() => {
+    return (
+        <>
             <UpDown type="normal">
                 <SVG icon="triangle" width={48} stroke left="10%" top="20%" />
                 <SVG icon="hexa" width={48} stroke left="60%" top="70%" />
+
                 <SVG icon="box" width={6} left="60%" top="15%" />
             </UpDown>
             <UpDown type="wide">
                 <SVG icon="triangle" width={24} stroke left="65%" top="8%" />
+
                 <SVG icon="triangle" width={12} stroke left="90%" top="50%" />
+
                 <SVG icon="triangle" width={16} stroke left="30%" top="65%" />
             </UpDown>
             <UpDown type="slow">
@@ -61,13 +80,6 @@ const AppLayout = ({ children }: LayoutProps): JSX.Element => {
                 <SVG icon="circle" width={6} left="4%" top="20%" />
                 <SVG icon="circle" width={12} left="50%" top="60%" />
             </UpDown>
-            <div className="flex flex-col justify-between min-h-screen text-black dark:text-white pageBackground backdrop-filter backdrop-blur-sm dark:backdrop-blur-lg">
-                <ThemeSwitcher />
-                {children}
-                {!router.asPath.includes("/t/") && <Footer />}
-            </div>
         </>
     );
-};
-
-export default AppLayout;
+});
