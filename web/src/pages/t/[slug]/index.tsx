03d6a299461ccc4ca24528bbd0de6213d957f64a
David Alexandru Ilie <david@davidilie.com>
2021-10-31 18:47:36 +0100

    update meta

--- web/src/pages/t/[slug]/index.tsx
+++ web/src/pages/t/[slug]/index.tsx
@@ -83,8 +83,8 @@ const ViewFile = ({ message, file, text }: Props): JSX.Element => {
                 canonical={router.basePath}
                 description={`${file.name} - ${file.stats.size} - MD5: ${file.stats.md5}`}
                 openGraph={{
-                    title: file.name,
-                    site_name: settings.name || "ShareX Upload Server",
+                    title: `${text.split(/\s+/gu)[0]}...`,
+                    site_name: settings.name,
                     description: `${file.name} - ${file.stats.size} - MD5: ${file.stats.md5}`,
                     url: router.basePath,
                     type: "website",
